b327fc1781d73a8bfa741b64c618b2c972b15d73
Ryan Higgins <c16381886@mydit.ie>
2017-11-22 19:51:59 +0000

    Rooms with traps

---
+++ Roguelike/Assets/trapSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapSpawn : MonoBehaviour {
+	public List<GameObject> spawns;
+	public List<GameObject> traps;
+	GameObject trapToChose;
+	bool generating;
+	int count;
+
+	// Use this for initialization
+	void Start () {
+		generating = true;
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (generating = true && count < 2) {
+			for (int i = 0; i < spawns.Count; i++) {
+				trapToChose = traps[Random.Range(0,traps.Count)];
+				trapToChose.transform.position = spawns [i].transform.position;
+				GameObject currentTrap = GameObject.Instantiate (trapToChose, spawns [i].transform, spawns [i]);
+				currentTrap.transform.position = spawns [i].transform.position;
+				count = count + 1;
+			}
+		}
+	}
+}
