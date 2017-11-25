f4073875647cc2a713fc933e49ac46cea3d4dcbf
Ryan Higgins <c16381886@mydit.ie>
2017-11-25 23:42:09 +0000

    Fire Trap partially working

---
+++ Roguelike/Assets/Scripts/fireTrap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireTrap : MonoBehaviour {
+
+	GameObject player;
+	public player playerHealth;
+
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		playerHealth = player.GetComponent<player> ();
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D entity) {
+		if (entity.tag == "Player") {
+			playerHealth.health -= 1;
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Roguelike/Assets/Scripts/player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		health = 10;	
+	}
+}
