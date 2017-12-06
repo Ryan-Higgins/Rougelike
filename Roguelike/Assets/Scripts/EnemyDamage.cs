4b7ce082670b92491f2fa9311888b5610fd751bd
Ryan Higgins <c16381886@mydit.ie>
2017-12-06 10:20:05 +0000

    Enemies can now damage players and change direction if they hit walls.

---
+++ Roguelike/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+	GameObject player;
+	player myPlayer;
+	public bool test = false;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		myPlayer = player.GetComponent<player> ();
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collision2D entity) {
+		test = true;
+
+			myPlayer.health -= 2;
+			gameObject.SetActive (false);
+
+	}
+}
