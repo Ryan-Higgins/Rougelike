c107113d08adfedbeaab88066839054b0dc2a58a
cjbarron <conorjbarron@gmail.com>
2017-12-12 21:09:10 +0000

    Updated Sprites + Character and Trap Simple Animations

--- Roguelike/Assets/Scripts/attack.cs
+++ Roguelike/Assets/Scripts/attack.cs
@@ -14,5 +14,6 @@ public class attack : MonoBehaviour {
 		if (Input.GetKey (KeyCode.Space)) {
 			Destroy (gameObject);
 		}
+		
 	}
 }
--- Roguelike/Assets/Scripts/move.cs
+++ Roguelike/Assets/Scripts/move.cs
@@ -8,12 +8,41 @@ public class move : MonoBehaviour {
 	Rigidbody2D myRB;
 	float h;
 	float v;
+	Animator anim;
+	bool wait = false;
+	bool stop = false;
+	bool stab;
+	public bool showObject;
+	public GameObject objectToShow;
 
 	void Start() {
 		myRB = GetComponent<Rigidbody2D> ();
+		anim = GetComponentInChildren<Animator>();
+		//StartCoroutine(WaitCoroutine());
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Wall" && stab == true){
+			anim.SetBool ("attackEnemy", false);
+		}
 	}
 
 	void FixedUpdate() {
+
+		stab = Input.GetKey (KeyCode.Space);
+
+		if (stab == true) {
+			anim.SetBool ("attackEnemy", true);
+			if(showObject){
+				objectToShow.SetActive (true);
+			}
+		}
+		if (stab == false) {
+			anim.SetBool ("attackEnemy", false);
+			if(showObject){
+				objectToShow.SetActive (false);
+			}
+		}
 
 		h = Input.GetAxis ("Horizontal");
 		v = Input.GetAxis ("Vertical");
@@ -21,14 +50,28 @@ public class move : MonoBehaviour {
 		Vector2 movement = new Vector2 (h, v);
 		myRB.AddForce (movement * speed);
 
-		if (Input.GetKey (KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			anim.SetBool ("walk", true);
 			transform.rotation = Quaternion.Euler (0, 0, 90);
-		} else if (Input.GetKey (KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+		} else if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			anim.SetBool ("walk", true);
 			transform.rotation = Quaternion.Euler (0, 0, 270);
-		} else if (Input.GetKey (KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+		} else if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+			anim.SetBool ("walk", true);
 			transform.rotation = Quaternion.Euler (0, 0, 0);
-		} else if (Input.GetKey (KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-			transform.rotation = Quaternion.Euler(0,0,180);
+		} else if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+			anim.SetBool ("walk", true);
+			transform.rotation = Quaternion.Euler (0, 0, 180);
+		} else {
+			anim.SetBool ("walk", false);
 		}
 	}
+
+	/*IEnumerator WaitCoroutine()
+	{
+		if (wait == true) {
+			yield return new WaitForSeconds (2f);
+			stop = true;
+		}
+	}*/
 }
