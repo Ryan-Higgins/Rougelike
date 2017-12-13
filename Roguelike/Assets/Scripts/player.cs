c6b45848ade7596c94b4e02398eb9e8bfb79c567
Ryan Higgins <c16381886@mydit.ie>
2017-12-13 16:48:39 +0000

    Post state of play commit

--- Roguelike/Assets/Scripts/player.cs
+++ Roguelike/Assets/Scripts/player.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour {
 	public float health;
 	public bool isBurning;
 	public bool isWet;
+	public Text healthText;
+	public bool hasKey;
 
 	// Use this for initialization
 	void Start () {
 		health = 10f;	
 		isBurning = false;
 		isWet = false;
+	}
+
+	void Update () {
+		healthText.text = health.ToString();
 	}
 }
